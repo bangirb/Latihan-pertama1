41695dd0654380b8504a97a7358209e21de580a5
bangirb <146005548+bangirb@users.noreply.github.com>
2024-09-07 23:35:14 +0700

    Add function to swap two variables
    Added a script to swap two variables (manggis and pisang) using a temporary variable.

---
+++ tukar_variabel.py
@@ -0,0 +1,11 @@
+# Input variabel
+x = "manggis"
+y = "pisang"
+
+# Gunakan variabel sementara untuk pertukaran
+temp = x
+x = y
+y = temp
+
+# Output hasil setelah pertukaran
+print(f'Posisi setelah ditukar: x = {x}, y = {y}')
