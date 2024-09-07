a5b2887b5807f92e4cb8fb188895f1c710f66953
bangirb <146005548+bangirb@users.noreply.github.com>
2024-09-08 00:00:01 +0700

    Update volume_tabung.py

--- volume_tabung.py
+++ volume_tabung.py
@@ -1,11 +1,8 @@
 import math
 
-# Input jari-jari dan tinggi tabung
 jari_jari = 3
 tinggi = 5
 
-# Hitung volume tabung dengan rumus: pi * r^2 * tinggi
 volume = math.pi * (jari_jari ** 2) * tinggi
 
-# Output hasil volume tabung
 print(f'Volume tabung dengan jari-jari {jari_jari} dan tinggi {tinggi} adalah {volume:.2f}')
