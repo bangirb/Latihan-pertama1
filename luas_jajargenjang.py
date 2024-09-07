dab0ac13319d5431d7903c5063332afab127e99c
bangirb <146005548+bangirb@users.noreply.github.com>
2024-09-07 23:58:38 +0700

    Update luas_jajargenjang.py

--- luas_jajargenjang.py
+++ luas_jajargenjang.py
@@ -1,9 +1,6 @@
-# Input panjang dan tinggi jajar genjang
 panjang = 5
 tinggi = 3
 
-# Hitung luas jajar genjang dengan rumus: panjang * tinggi
 luas = panjang * tinggi
 
-# Output hasil luas jajar genjang
 print(f'Luas jajar genjang dengan panjang {panjang} dan tinggi {tinggi} adalah {luas}')
