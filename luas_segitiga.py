c8c307ddaa59f8790e5e107ab0c1e66833fce491
bangirb <146005548+bangirb@users.noreply.github.com>
2024-09-07 23:59:08 +0700

    Update luas_segitiga.py

--- luas_segitiga.py
+++ luas_segitiga.py
@@ -1,9 +1,6 @@
-# Input alas dan tinggi segitiga
 alas = 25
 tinggi = 30
 
-# Hitung luas segitiga dengan rumus: 0.5 * alas * tinggi
 luas = 0.5 * alas * tinggi
 
-# Output hasil luas segitiga
 print(f'Luas segitiga dengan alas {alas} dan tinggi {tinggi} adalah {luas}')
