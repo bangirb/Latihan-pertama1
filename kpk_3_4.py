688f1deaff215e5d84f0b859184662e705790545
bangirb <146005548+bangirb@users.noreply.github.com>
2024-09-07 23:57:27 +0700

    Update kpk_3_4.py

--- kpk_3_4.py
+++ kpk_3_4.py
@@ -1,11 +1,8 @@
 import math
 
-# Input nilai a dan b
 a = 3
 b = 4
 
-# Hitung KPK menggunakan rumus KPK = (a * b) // FPB
 kpk = (a * b) // math.gcd(a, b)
 
-# Output hasil KPK
 print(f'KPK dari {a} dan {b} adalah {kpk}')
